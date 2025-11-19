f78c02eed46d1bb79a4aa4a71951c298bb434bb5
krishnav kishor <krishnav.k7k7@gmail.com>
2025-11-20 01:53:55 +0530

    Updated API URL

--- frontend/src/services/taskService.js
+++ frontend/src/services/taskService.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api/tasks';
+const BASE_URL = process.env.REACT_APP_API_URL || 'https://task-manager-2xhv.onrender.com';
 
-export const getTasks = (params) => axios.get(API_URL, { params });
-export const getTask = (id) => axios.get(`${API_URL}/${id}`);
-export const createTask = (data) => axios.post(API_URL, data);
-export const updateTask = (id, data) => axios.put(`${API_URL}/${id}`, data);
-export const deleteTask = (id) => axios.delete(`${API_URL}/${id}`);
+export const getTasks = (params) => axios.get(`${BASE_URL}/api/tasks`, { params });
+export const getTask = (id) => axios.get(`${BASE_URL}/api/tasks/${id}`);
+export const createTask = (data) => axios.post(`${BASE_URL}/api/tasks`, data);
+export const updateTask = (id, data) => axios.put(`${BASE_URL}/api/tasks/${id}`, data);
+export const deleteTask = (id) => axios.delete(`${BASE_URL}/api/tasks/${id}`);
